7294b1fba9b0a5584b33ace9d2352ded4807a90c
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-11-05 15:50:44 -0500

    objects

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -50,7 +50,12 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          touchAction="none"
+        />
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
